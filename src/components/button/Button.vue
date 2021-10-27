7ab83f13e152dbc147198d87d012f9706e6156d5
seongsil <yooo1201@naver.com>
2021-10-27 14:46:28 +0900

    add: button component

---
+++ src/components/button/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button type="button" class="c-button" :class="color">{{ value }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '확인',
+    },
+    color: {
+      type: String,
+      default: 'primary',
+    }
+  }
+}
+</script>
+
+<style>
+.c-button.primary {
+  background: green;
+}
+.c-button.error {
+  background: indianred;
+}
+.c-button.success {
+  background: dodgerblue;
+}
+.c-button.secondary {
+  background: lightslategray;
+}
+</style>
