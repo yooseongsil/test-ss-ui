b239070958ecedfd0c2d6e1d5a566b9a7a22e991
seongsil <yooo1201@naver.com>
2021-11-01 17:24:40 +0900

    add: 레거시버튼 컴포넌트

---
+++ src/components/button/LegacyButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>레거시버튼 테스트</button>
+</template>
+
+<script>
+/**
+ * 레거시 버튼
+ * @displayName s-legacy-button
+ * @deprecated
+ */
+export default {
+  name: 'LegacyButton',
+}
+</script>
+
+<style>
+
+</style>
